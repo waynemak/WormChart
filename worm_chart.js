eb819288d390dd655304b3d0ecbdb63c48112dc3
Ryan Weald <ryan@weald.com>
2012-01-29 14:58:24 -0800

    chart will new respond to click events and trigger events on parent

--- worm_chart.js
+++ worm_chart.js
@@ -1,18 +1,36 @@
 var WormChart = function(args) {
   this.positive = args["positive"];
   this.negative = args["negative"];
+  this.container = args["container"];
+
+
 
   this.render = function(){
     $("#worm_chart .positive").css("height", "" + this.positive + "%");
     $("#worm_chart .negative").css("height", "" + this.negative + "%");
-  }
+  };
 
   this.updateNegative = function(newNegative) {
+    console.log(newNegative);
     this.positive = 100 - newNegative;
     this.negative = newNegative;
     this.render();
+
+    return newNegative;
   };
 
+  this.wormHtml = '<div id="worm_chart"> <div class="negative"></div> <div class="positive"> </div> </div>';
+  
+  $(this.container).html(this.wormHtml);
+
   this.render();
+
+  var self = this;
+  $("#worm_chart").click(function(){
+    console.log(event.y);
+    var newNeg = self.updateNegative(Math.floor((event.y / 250.0) * 100));
+    $(self.container).trigger("chartUpdated", newNeg);
+  });
+  
   return this;
 };
