f50eee203f7c4399cfc5817bf65d8c7225e6a6a0
Ryan Weald <ryan@weald.com>
2012-02-01 19:38:23 -0800

    remove some functionality we aren't going to need.

--- worm_chart.js
+++ worm_chart.js
@@ -9,31 +9,7 @@ var WormChart = function(args) {
     this.height = parseFloat($(this.container).css("height"));
   };
 
-  this.animateUpdate = function() { 
-    jQuery("#worm_chart .positive").animate({
-      "height": "" + this.positive + "%"
-    }, "slow");
 
-    jQuery("#worm_chart .negative").animate({
-      "height": "" + this.negative + "%"
-    }, "slow");
-  };
-
-  this.updateValue = function(newPosValue) {
-    this.positive = newPosValue;
-    this.negative = 100 - newPosValue;
-    this.animateUpdate();
-
-    return newPosValue;
-  };
-
-  this.bindToClick = function() { 
-    //var self = this;
-    //jQuery("#worm_chart").click(function(){
-      //var newPos = self.updateValue(Math.ceil(((this.height - event.y) / this.height.0) * 100));
-      //jQuery(self.container).trigger("chartUpdated", newPos);
-    //});
-  };
 
   this.wormHtml = '<div id="worm_chart"> <div class="negative"></div> <div class="positive"> </div> </div>';
   
