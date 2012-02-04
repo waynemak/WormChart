892406af032b3e0bebd1de9ec86f38e0610d0b00
Ryan Weald <ryan@weald.com>
2012-02-04 11:52:20 -0800

    wormChart is now a jQuery plugin. No longer a global object instead you can just call it using jQuery.

--- worm_chart.js
+++ worm_chart.js
@@ -1,26 +1,35 @@
-var WormChart = function(args) {
-  //this.positive = args["positive"];
-  //this.negative = 100 - this.positive;
-  this.container = args["el"];
-  this.data = args["data"];
+(function($) {
+  var WormChart = function(args) {
+    //this.positive = args["positive"];
+    //this.negative = 100 - this.positive;
+    this.container = args["el"];
+    this.data = args["data"];
 
-  this.htmlPortion = function(size, portionNumber) {
-    return '<div class="worm_portion portion_color' + (portionNumber % 3) + '" style="width:' + size + '%;"></div>'
-  }
-
-  this.render = function(){
-    var dataLength = this.data.length;
-    var innerHtml = ""
-    for(var i = 0; i < dataLength; i ++){
-      innerHtml += this.htmlPortion(this.data[i], i)
-
+    this.htmlPortion = function(size, portionNumber) {
+      return '<div class="worm_portion portion_color' + (portionNumber % 3) + '" style="width:' + size + '%;"></div>'
     }
 
-    jQuery(this.container).html('<div class="worm_chart">' + 
-                                innerHtml + '</div>');
-  };
+    this.render = function(){
+      var dataLength = this.data.length;
+      var innerHtml = ""
+      for(var i = 0; i < dataLength; i ++){
+        innerHtml += this.htmlPortion(this.data[i], i)
 
-  this.render();
+      }
 
-  return this;
-};
+      jQuery(this.container).html('<div class="worm_chart">' + 
+                                  innerHtml + '</div>');
+    };
+
+    this.render();
+
+    return this;
+  }
+
+  $.fn.wormChart = function(data){
+    new WormChart({
+      "el" : this,
+      "data" : data
+    });
+  }
+})(jQuery);
